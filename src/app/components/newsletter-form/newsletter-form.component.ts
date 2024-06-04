39545ce01d8a65c77ef0e16199c66386d8150290
AllanVargens <allanvargens@gmail.com>
2024-06-03 22:20:51 -0300

    feat: Project LandingPage - Angular17

---
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -0,0 +1,45 @@
+import { Component, signal } from '@angular/core';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NewsletterService } from '../../services/newsletter.service';
+
+@Component({
+  selector: 'app-newsletter-form',
+  standalone: true,
+  imports: [BtnPrimaryComponent, ReactiveFormsModule],
+  providers: [NewsletterService],
+  templateUrl: './newsletter-form.component.html',
+  styleUrl: './newsletter-form.component.scss',
+})
+export class NewsletterFormComponent {
+  newsletterForm!: FormGroup;
+  loading = signal(false);
+
+  constructor(private newsletterService: NewsletterService) {
+    this.newsletterForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+  onSubmit() {
+    this.loading.set(true);
+    if (this.newsletterForm.valid) {
+      this.newsletterService
+        .sendData(
+          this.newsletterForm.value.name,
+          this.newsletterForm.value.name
+        )
+        .subscribe({
+          next: () => {
+            this.newsletterForm.reset();
+            this.loading.set(false);
+          },
+        });
+    }
+  }
+}
